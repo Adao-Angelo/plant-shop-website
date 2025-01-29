dd433b1b1ac37db7333ebfea7db686174d61ffe9
Adao-Angelo <adaobegginer@gmail.com>
2025-01-29 19:46:39 +0100

    init setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AnimatedArrowLeft from "./components/AnimatedArrow/AnimatedArrowLeft";
+import AnimatedArrowRight from "./components/AnimatedArrow/AnimatedArrowRight";
+
+function App() {
+  return (
+    <>
+      <AnimatedArrowLeft />
+      <AnimatedArrowRight />
+    </>
+  );
+}
+
+export default App;
