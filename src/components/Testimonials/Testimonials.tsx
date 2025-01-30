562a931aac4f540721e8d1ac1c2ab94bdb36c730
Adao-Angelo <adaobegginer@gmail.com>
2025-01-30 16:54:43 +0100

    create footer

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -6,7 +6,7 @@ export default function Testimonials() {
   const { testimonials } = useTestimonialsStore();
 
   return (
-    <section className="mt-[11.3rem] mb-[30rem]">
+    <section className="mt-[11.3rem] mb-[20rem]">
       <h1 className="font-bold text-[3.2rem] w-[45.3rem]">
         What customers say about GREE<span>MIND?</span>
       </h1>
